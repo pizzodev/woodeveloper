7bd22fab1191bbb57c3d4e3815e24e763ca7a777
andrea.gazzi <andrea.gazzi@adesso.ch>
2025-12-07 23:23:18 +0100

    Foto

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const FooterComponent: React.FC = () => {
     return (
         <footer className="bg-white dark:bg-zinc-900 mt-16 py-6 text-center text-zinc-600 dark:text-zinc-400">
-            &copy; {new Date().getFullYear()} WooDeveloper. All rights reserved.
+            &copy; {new Date().getFullYear()} woodeveloper. Tutti i diritti riservati.
         </footer>
     )
 }
--- app/components/products/data/products.js
+++ app/components/products/data/products.js
@@ -3,24 +3,24 @@ export const products = [
         id: 1,
         name: "Albero di Natale",
         price: "€ 20",
-        image: "/products/logo.png",
+        image: "/products/albero.jpg",
     },
     {
         id: 2,
         name: "Tagliere",
         price: "€ 30",
-        image: "/products/logo.png",
+        image: "/products/tagliere.png",
     },
     {
         id: 3,
         name: "Orologio",
         price: "€ 50",
-        image: "/products/logo.png",
+        image: "/products/orologio.png",
     },
     {
         id: 4,
         name: "Portavaso",
         price: "€ 10",
-        image: "/products/logo.png",
+        image: "/products/portavaso.png",
     },
 ];
