64d77c8124ba24ea5d26485fe5e042d69c2a4d8f
andrea.gazzi <andrea.gazzi@adesso.ch>
2025-12-21 15:10:40 +0100

    Reshuffle translations

--- app/data/products/products.js
+++ app/data/products/products.js
@@ -2,25 +2,25 @@ export const products = [
     {
         id: 1,
         name: "Albero di Natale",
-        price: "€ 20",
+        price: "20",
         image: "/products/albero.jpg",
     },
     {
         id: 2,
         name: "Tagliere",
-        price: "€ 30",
+        price: "30",
         image: "/products/tagliere.png",
     },
     {
         id: 3,
         name: "Orologio",
-        price: "€ 50",
+        price: "50",
         image: "/products/orologio.png",
     },
     {
         id: 4,
         name: "Portavaso",
-        price: "€ 10",
+        price: "10",
         image: "/products/portavaso.png",
     },
 ];
