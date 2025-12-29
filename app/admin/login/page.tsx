df653ea7896bf5eb583a9fc55a136110c4c9061c
andrea.gazzi <andrea.gazzi@adesso.ch>
2025-12-29 16:02:42 +0100

    Login ripristinato

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -6,7 +6,7 @@ export default function AdminAddProductPage() {
     return (
         <div className="bg-zinc-50 dark:bg-black min-h-screen font-sans">
             <ScaffoldComponent backgroundVideo={"videos/forest.mp4"}>
-                <AdminAddProductComponent/>
+                <AdminLogin/>
             </ScaffoldComponent>
         </div>
     );
